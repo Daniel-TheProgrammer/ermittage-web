e7f5fd8a2f005d383c209b21d7a86e9e0a58724e
Daniel-TheProgrammer <njid18753@gmail.com>
2024-08-20 07:33:17 +0100

    feat: layout

--- function.js
+++ function.js
@@ -1,10 +1,29 @@
-
-
-
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger, Observer, MotionPathPlugin);
+  const heroContainer = document.querySelector(".header__container");
+  const headerSticky = document.querySelector(".header_sticky");
+  const children = heroContainer.children;
 
+  let secondChild = children[2];
+  let secondChildTop = secondChild.offsetTop;
 
-  //
+  const initialWidth = 691; // Initial width of the image
+  const finalWidth = 1163; // Final width of the image
 
+  function handleScroll(event) {
+    const scalingImage = document.querySelector(".header_sticky img");
+
+    const scrollPosition = window.scrollY;
+
+    let widthValue =
+      initialWidth +
+      (finalWidth - initialWidth) * (scrollPosition / secondChildTop);
+
+    if (scrollPosition >= secondChildTop) {
+      widthValue = finalWidth;
+    }
+
+    scalingImage.style.width = `${widthValue}px`;
+  }
+
+  window.addEventListener("scroll", handleScroll, { passive: true });
 });
